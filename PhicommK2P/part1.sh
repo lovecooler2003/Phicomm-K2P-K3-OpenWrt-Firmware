dbf0b0c0fdd54cb0c1f60f6f3fea6838bca6b1fc
lovecooler2003 <61578796+lovecooler2003@users.noreply.github.com>
2022-04-18 01:47:16 +0800

    Update part1.sh

--- PhicommK2P/part1.sh
+++ PhicommK2P/part1.sh
@@ -17,6 +17,7 @@
 #
 
 echo '添加SSRPLUS软件源'
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 cat feeds.conf.default
 echo '=========Add feed source OK!========='
