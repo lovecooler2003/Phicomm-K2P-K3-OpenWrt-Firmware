31a0f637a71ea8c244132e299c393bb70fbaf53f
lovecooler2003 <61578796+lovecooler2003@users.noreply.github.com>
2022-04-19 11:52:55 +0800

    Update part1.sh

--- PhicommK2P/part1.sh
+++ PhicommK2P/part1.sh
@@ -17,7 +17,7 @@
 #
 
 echo '添加SSRPLUS软件源'
-sed -i '$a src-git bearking https://github.com/bearking1069/openwrt-packages' feeds.conf.default
+sed -i '$a src-git OpenWrt-packages https://github.com/bearking1069/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 cat feeds.conf.default
